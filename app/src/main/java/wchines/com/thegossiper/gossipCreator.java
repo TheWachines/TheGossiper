b584a3032a2166f997829623c2348f3cb796eedb
Ricardo Fretes <rgfretes@gmail.com>
2016-09-04 12:58:01 +1200

    created basic uis

--- app/src/main/java/wchines/com/thegossiper/gossipCreator.java
+++ app/src/main/java/wchines/com/thegossiper/gossipCreator.java
@@ -6,16 +6,15 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class HomeActivity extends AppCompatActivity {
+public class gossipCreator extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-    }
+        setContentView(R.layout.activity_gossip_creator);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
 
 }
