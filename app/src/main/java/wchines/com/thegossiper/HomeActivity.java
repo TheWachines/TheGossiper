e0e88b982d08675ed8bc09f48b591f2841b46064
Ricardo Fretes <rgfretes@gmail.com>
2016-09-07 18:55:34 +1200

    Added facebook sdk and login button to main view

--- app/src/main/java/wchines/com/thegossiper/HomeActivity.java
+++ app/src/main/java/wchines/com/thegossiper/HomeActivity.java
@@ -1,21 +1,132 @@
 package wchines.com.thegossiper;
 
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class HomeActivity extends AppCompatActivity {
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.ProfilePictureView;
+
+public class homeActivity extends AppCompatActivity {
+
+    private ProfilePictureView profilePictureView;
+    private PendingAction pendingAction = PendingAction.NONE;
+    private CallbackManager callbackManager;
+    private ProfileTracker profileTracker;
+
+    private enum PendingAction {
+        NONE,
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        FacebookSdk.sdkInitialize(this.getApplicationContext());
+
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+
+                        Intent intent = new Intent(homeActivity.this,gossipCreator.class );
+                        intent.setData(Uri.parse("http://www.example.com/image.png"));
+                        startService(intent);
+
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        showAlert();
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        showAlert();
+                    }
+
+                    private void showAlert() {
+                        new AlertDialog.Builder(homeActivity.this)
+                                .setTitle(R.string.cancelled)
+                                .setMessage(R.string.permission_not_granted)
+                                .setPositiveButton(R.string.ok, null)
+                                .show();
+                    }
+                });
+
         setContentView(R.layout.activity_home);
+
+        profileTracker = new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(Profile oldProfile, Profile currentProfile) {
+                updateUI();
+                // It's possible that we were waiting for Profile to be populated in order to
+                // post a status update.
+                handlePendingAction();
+            }
+        };
+
+        profilePictureView = (ProfilePictureView) findViewById(R.id.profilePicture);
+
     }
 
 
+    public void updateUI() {
+        // Call second View
+        // Executed in an Activity, so 'this' is the Context
+        // The fileUrl is a string URL, such as "http://www.example.com/image.png"
+        Intent intent = new Intent(homeActivity.this,gossipCreator.class );
+        startService(intent);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        profileTracker.stopTracking();
+    }
+
+
+    private void handlePendingAction() {
+        PendingAction previouslyPendingAction = pendingAction;
+        // These actions may re-set pendingAction if they are still pending, but we assume they
+        // will succeed.
+        pendingAction = PendingAction.NONE;
+
+        switch (previouslyPendingAction) {
+            case NONE:
+                break;
+
+        }
+    }
 }
--- app/src/main/java/wchines/com/thegossiper/gossipCreator.java
+++ app/src/main/java/wchines/com/thegossiper/gossipCreator.java
@@ -1,11 +1,7 @@
 package wchines.com.thegossiper;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class gossipCreator extends AppCompatActivity {
 
@@ -13,8 +9,7 @@ public class gossipCreator extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gossip_creator);
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
 
 }
